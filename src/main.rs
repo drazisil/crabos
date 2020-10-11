15fa606dfcd822986d4df3cd231d1b166b2ef17a
Joe Becher <me@drazisil.com>
2020-10-11 08:00:22 -0400

    main works with lib

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,43 @@
 #![no_main]
 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(crabos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-#[macro_use(lazy_static)]
-extern crate lazy_static;
+// #[macro_use(lazy_static)]
+// extern crate lazy_static;
 
-mod vga_buffer;
-mod serial;
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
+// #[cfg(test)]
+// fn test_runner(tests: &[&dyn Fn()]) {
+//     serial_println!("Running {} tests", tests.len());
+//     for test in tests {
+//         test();
+//     }
+//
+//     crabos::exit_qemu(crabos::QemuExitCode::Success);
+// }
 
 use core::panic::PanicInfo;
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    crabos::println!("{}", info);
     loop {}
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
+    crabos::serial_println!("[failed]\n");
+    crabos::serial_println!("Error: {}\n", info);
+    crabos::exit_qemu(crabos::QemuExitCode::Failed);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    crabos::println!("Hello World{}", "!");
 
     #[cfg(test)]
         test_main();
@@ -51,23 +48,7 @@ pub extern "C" fn _start() -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
+    crabos::serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+    crabos::serial_println!("[ok]");
 }
