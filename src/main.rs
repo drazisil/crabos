f5ede426abf6923f8d6ff5e0bfcd97a5842f46b5
Joe Becher <me@drazisil.com>
2020-10-24 12:21:10 -0400

    A new hope

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
 #![no_std]
 #![no_main]
 
-#![feature(custom_test_frameworks)]
-#![test_runner(crabos::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
 use core::panic::PanicInfo;
-use crabos::println;
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    crabos::test_panic_handler(info)
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    loop {
 
-    #[cfg(test)]
-    test_main();
-
-    loop {}
+    }
 }
 
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+
+    }
 }
