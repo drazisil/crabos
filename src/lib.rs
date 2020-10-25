a5a39e09f1e6f5582cd5975d2a4c4158c979282a
Joe Becher <me@drazisil.com>
2020-10-25 16:52:24 -0400

    Double fault handler works

--- src/lib.rs
+++ src/lib.rs
@@ -20,12 +20,27 @@ pub fn init() {
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(multiboot_information_address: usize) -> ! {
 
     vga_buffer::clear_screen();
     println!("Hello World{}", "!");
 
-    // init();
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}",
+                 area.base_addr, area.length);
+    }
+
+    init();
+
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     hlt_loop()
 }
