5de4626d84836054f0fd86f57b4f4513885a72b2
Joe Becher <me@drazisil.com>
2020-10-25 08:09:41 -0400

    The vga_buffer works

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,22 @@
 
 use core::panic::PanicInfo;
 
+// Since this is a library, we need to import the lazy_static macro here
+#[macro_use(lazy_static)]
+extern crate lazy_static;
+
+// Import the vga_buffer file
+mod vga_buffer;
+
+use crate::vga_buffer::WRITER;
+
+
 #[no_mangle]
 pub extern "C" fn _start() {
-    // loop {
-    //
-    // }
+    cls!();
+    println!("Hello World{}", "!");
+
+    // loop {}
 }
 
 #[panic_handler]
